e0857438d0903b2cd5ad9e3d7015ded9d6c7dbdc
Alisher Zakir <a3zakir@gmail.com>
2019-04-22 00:49:43 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,7 +6,7 @@ end
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
-define won? (board)
+define won?(board)
 WIN_COMBINATION.each do |comb|
   return comb if board[comb[0]] == board[comb[1]] && board[comb[0]] == board[comb[2]]
 end
